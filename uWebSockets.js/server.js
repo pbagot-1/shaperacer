3dfee3f730bc0f5f3d7cb22df62c07a46d438021
prodscary <46110722+prodscary@users.noreply.github.com>
2021-05-23 17:38:22 -0700

    got multiple games running at once, need to do end of game handling

--- uWebSockets.js/server.js
+++ uWebSockets.js/server.js
@@ -1,13 +1,11 @@
 // npm install uNetworking/uWebSockets.js#v16.2.0
 const uWS = require('uWebSockets.js');
 const importLinked = require("./ExpiringLinkedList.js");
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+const shapeGen = require("./shapegen.js");
 
 const decoder = new TextDecoder();
 
-var flag = false;
+//var flag = false;
 var count = 0;
 var mainLinkedList = new importLinked.ExpiringLinkedList(3);
 
@@ -22,6 +20,14 @@ var diffy = 0;
 function create_UUID(a, b) { for (b = a = ''; a++ < 36; b += a * 51 & 52 ? (a ^ 15 ? 8 ^ Math.random() * (a ^ 20 ? 16 : 4) : 4).toString(16) : '-'); return b }
 function createRoomName() {return 'lobby' + create_UUID();}
 
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
 const mapToObj = m => {
   return Array.from(m).reduce((obj, [key, value]) => {
     obj[key] = value;
@@ -29,29 +35,32 @@ const mapToObj = m => {
   }, {});
 };
 
+function generateNewRoom(roomName, socketList) {
+    newScoreMap = new Map();
+    newLinkedList = new importLinked.ExpiringLinkedList(3);
+    socketList.forEach(item => newScoreMap.set(item.uuid, 0));
+    gameMap.set(roomName, [newScoreMap, newLinkedList]);
+}
+
 function check(socket, message, app) {
     var found = false;
     function socketCheck(item) {
         //TODO: use iterator to stop early instead of each
         if (found == false) {
             if (item[0] == message) {
-                            console.log("GANG", Array.from(item[1].keys()));
-                            console.log("cursok", socket.uuid);
-                            console.log("valting", item[1].get(socket.uuid));
-                             console.log("in",socket.uuid in Array.from(item[1].keys()));
                 if (Array.from(item[1].keys()).includes(socket.uuid) && item[1].get(socket.uuid) == false) {
-                    console.log("WTF");
-                scoreMap.set(socket.uuid, scoreMap.get(socket.uuid) + 1);
-                item[1].set(socket.uuid, true);
-                console.log("current score of " + socket.uuid + " " + scoreMap.get(socket.uuid));
-                found = true;
-                
-                //var scoreMapUpdate = new Map([...scoreMap.entries()].sort());
-                //console.log("SMU", scoreMapUpdate);
-                msg = JSON.stringify(["scoreboard", mapToObj(scoreMap)]);
-                //gng = JSON.parse(msg);
-                //console.log(gng);
-                app.publish("drawing/canvas1", msg, false); 
+                    currentScoreMap = gameMap.get(socket.room)[0];
+                    currentScoreMap.set(socket.uuid, currentScoreMap.get(socket.uuid) + 1);
+                    item[1].set(socket.uuid, true);
+                    console.log("current score of " + socket.uuid + " " + currentScoreMap.get(socket.uuid));
+                    found = true;
+                    
+                    //var scoreMapUpdate = new Map([...scoreMap.entries()].sort());
+                    //console.log("SMU", scoreMapUpdate);
+                    msg = JSON.stringify(["scoreboard", mapToObj(currentScoreMap)]);
+                    //gng = JSON.parse(msg);
+                    //console.log(gng);
+                    app.publish(socket.room, msg, false); 
                 }
             }
         }
@@ -59,156 +68,46 @@ function check(socket, message, app) {
     return socketCheck;
 }
 
-function shapeGen() {
-var choice = getRandomInt(6) + 1;    
+function startGameLoop(room) {
+    var flag = false;
+    var gameLoop = setInterval(function() {
+    if (flag == false) {
+        msg = JSON.stringify(["scoreboard", mapToObj(gameMap.get(room)[0])]);
+        //gng = JSON.parse(msg);
+        //console.log(gng);
+        app.publish(room, msg, false); 
+    }
+    flag = true;
+    shape = shapeGen.shapeGen(); 
+    shape2 = shapeGen.shapeGen();
+    shape3 = shapeGen.shapeGen();
+    app.publish(room, JSON.stringify(["drawing", shape[1]]), false); 
+    app.publish(room, JSON.stringify(["drawing", shape2[1]]), false); 
+    app.publish(room, JSON.stringify(["drawing", shape3[1]]), false); 
+    myDict = new Map();
+    var keys = Array.from(gameMap.get(room)[0].keys());
+    keys.forEach(function(key){
+    myDict.set(key, false);    
+    });
 
-switch (choice){
-  case 1:
-    // Square that starts somewhere randomly
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
+    myDict2 = new Map();
+    keys.forEach(function(key){
+    myDict2.set(key, false);    
+    });
 
-    var line1 = new Uint16Array([x, y]);
-
-    x = x + 50; 
-    var line2 = new Uint16Array([x, y]);
-
-    y = y + 50;
-    var line3 = new Uint16Array([x, y]);
-
-    x = x - 50;
-    var line4 = new Uint16Array([x, y]);
-
-    y = y - 50;
-    var line5 = new Uint16Array([x, y]);
-
-    var newList = new Uint16Array([...line1, ...line2, ...line3, ...line4, ...line5]);
-    return ["s", newList];
-  
-  case 2:
-    // Triangle
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
-
-    var line1 = new Uint16Array([x, y]);
-
-    x = x + 50; 
-    var line2 = new Uint16Array([x, y]);
-
-    x = x - 25;
-    y = y + 50;
-    var line3 = new Uint16Array([x, y]);
-
-    x = x - 25;
-    y = y - 50;
-    var line4 = new Uint16Array([x, y]);
-
-    var line5 = new Uint16Array([x, y]);
-
-    var newList = new Uint16Array([...line1, ...line2, ...line3, ...line4, ...line5]);
-    return ["t", newList];
+    myDict3 = new Map();
+    keys.forEach(function(key){
+    myDict3.set(key, false);    
+    });            
+    gameLinkedList = gameMap.get(room)[1];
+    gameLinkedList.push([shape[0], myDict]); 
+    gameLinkedList.push([shape2[0], myDict2]); 
+    gameLinkedList.push([shape3[0], myDict3]); 
+    count++; 
+    console.log(count);
+    }, 3000);
     
-  case 3:
-    // Triangle
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
-
-    var line1 = new Uint16Array([x, y]);
-    x = x + 25;
-    var line2 = new Uint16Array([x, y]);
-    x = x + 25;
-    y = y + 25;
-    var line3 = new Uint16Array([x, y]);
-    y = y + 25;
-    var line4 = new Uint16Array([x, y]);
-    x = x - 25;
-    y = y + 25;
-    var line5 = new Uint16Array([x, y]);
-    
-    x = x - 25;
-    var line6 = new Uint16Array([x, y]);
-    
-    x = x - 25;
-    y = y - 25;
-    var line7 = new Uint16Array([x, y]);
-
-    y = y - 25;
-    var line8 = new Uint16Array([x, y]);
-    
-    x = x + 25;
-    y = y - 25;
-    var line9 = new Uint16Array([x, y]);
-
-    var newList = new Uint16Array([...line1, ...line2, ...line3, ...line4, ...line5,  ...line6,  ...line7,  ...line8,  ...line9,]);
-    return ["o", newList];
-    
-  case 4:
-    // Arrow
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
-
-    var line1 = new Uint16Array([x, y]);
-    x = x + 25;
-    var line2 = new Uint16Array([x, y]);
-
-    y = y - 15;
-    var line3 = new Uint16Array([x, y]);
-    
-    x = x + 25
-    y = y + 27.5;
-    var line4 = new Uint16Array([x, y]);
-    
-    x = x - 25;
-    y = y + 27.5;
-    var line5 = new Uint16Array([x, y]);
-    
-    y = y - 15;
-    var line6 = new Uint16Array([x, y]);
-    
-    x = x - 25;
-    var line7 = new Uint16Array([x, y]);
-
-    y = y - 25;
-    var line8 = new Uint16Array([x, y]);
-
-    var newList = new Uint16Array([...line1, ...line2, ...line3, ...line4, ...line5,  ...line6,  ...line7,  ...line8]);
-    return ["a", newList];
-    
-  case 5:
-    // Diamond
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
-
-    var line1 = new Uint16Array([x, y]);
-    
-    x = x + 25;
-    y = y + 25;
-    var line2 = new Uint16Array([x, y]);
-
-    x = x - 25;
-    y = y + 25;
-    var line3 = new Uint16Array([x, y]);
-    
-    x = x - 25
-    y = y - 25;
-    var line4 = new Uint16Array([x, y]);
-    
-    x = x + 25;
-    y = y - 25;
-    var line5 = new Uint16Array([x, y]);
-
-    var newList = new Uint16Array([...line1, ...line2, ...line3, ...line4, ...line5]);
-    return ["d", newList];
-    
-  case 6:
-    // Circle
-    var x = getRandomInt(620) + 50;
-    var y = getRandomInt(380) + 50;
-
-    var line1 = new Uint16Array([x, y, 50, 0, 2 * Math.PI + 1]);
-    //ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    return ["c", line1];
-}
+    gameMap.get(room).push(gameLoop);
 }
 
 const app = uWS.App().ws('/*', {  // handle messages from client
@@ -258,35 +157,38 @@ const app = uWS.App().ws('/*', {  // handle messages from client
     var d = new Date();
     var n = d.getTime();
     
-    if (diffy != 0){
-    console.log(n);
-    console.log("TIME DIF IN RECIEVED", n - diffy);
-    diffy = n;
-    } else {
-    diffy = n;
-    }
-    
     m = decoder.decode(message);
     /* In this simplified example we only have drawing commands */
     console.log("from message: ", m, socket.uuid);
     if (m == "play") {
         console.log("Got to play");
-        searchingPlayers.push(socket.uuid);
+        searchingPlayers.push(socket);
         if (searchingPlayers.length == 2) {
-            searchingPlayers.forEach(item => (wsMap.get(item)).subscribe("testyroom"));
+            new_room = createRoomName();
+            searchingPlayers.forEach(item => {item.subscribe(new_room); item.room = new_room;});
+            generateNewRoom(new_room, searchingPlayers);
             searchingPlayers = [];
+            var z = new Date();
+            var x = z.getTime();
+            console.log("TIME DIF IN RECIEVED", x - n);
+            app.publish(new_room, JSON.stringify(["GAME START"]), false); 
+            startGameLoop(new_room);
         }
         
     }
-    app.publish("testyroom", "IN ROOM NOW!", false); 
-    //mainLinkedList.each(check(socket, m, app));
+    
+    // check to see if sockets score needs to be increased
+    if (typeof socket.room !== "undefined") {
+        gameMap.get(socket.room)[1].each(check(socket, m, app));
+    }
+    
   },
   
   close: (socket, code, message) => {
     // called when a ws connection is closed
-    scoreMap.delete(socket.uuid);
-    msg = JSON.stringify(["scoreboard", mapToObj(scoreMap)]);
-    app.publish("drawing/canvas1", msg, false); 
+    //scoreMap.delete(socket.uuid);
+    //msg = JSON.stringify(["scoreboard", mapToObj(scoreMap)]);
+    //app.publish("drawing/canvas1", msg, false); 
     console.log("CLOSED");
   }
 });
